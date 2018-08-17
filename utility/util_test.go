ad0fa41a15791b12459229e40d2c30ff630d5ae1
giuliocomi <giulio1.comi@mail.polimi.it>
2018-08-17 15:55:44 +0200

    Add some tests - gofmt formatted now

--- utility/util_test.go
+++ utility/util_test.go
@@ -10,12 +10,12 @@ func TestContainsPort(t *testing.T) {
 		param2   int
 		expected bool
 	}{
-		{[]int{1,22,3,5050,80,8080, 445}, 445, true},
-		{[]int{1,22,3,5050,80,8080, 445}, -22, false},
-		{[]int{1,22,3,5050,80,8080, 445}, 12321, false},
-		{[]int{1,22,3,5050,80,8080, 445}, 808080, false},
-                {[]int{1,22,22,22,80,8080, 445}, 22, true},
-                {[]int{1,22,3,5050,80,8080, 445000}, 445000, false},
+		{[]int{1, 22, 3, 5050, 80, 8080, 445}, 445, true},
+		{[]int{1, 22, 3, 5050, 80, 8080, 445}, -22, false},
+		{[]int{1, 22, 3, 5050, 80, 8080, 445}, 12321, false},
+		{[]int{1, 22, 3, 5050, 80, 8080, 445}, 808080, false},
+		{[]int{1, 22, 22, 22, 80, 8080, 445}, 22, true},
+		{[]int{1, 22, 3, 5050, 80, 8080, 445000}, 445000, false},
 	}
 	for _, test := range tests {
 		actual := ContainsPort(test.param1, test.param2)
@@ -29,7 +29,7 @@ func TestIsValidIP4(t *testing.T) {
 	t.Parallel()
 
 	var tests = []struct {
-		param   string
+		param    string
 		expected bool
 	}{
 		{"127.0.0.1", true},
@@ -39,7 +39,7 @@ func TestIsValidIP4(t *testing.T) {
 		{"-151.12.29.30", false},
 		{"127ssee.0.0.1", false},
 		{"127.0.01", false},
-                {"::1", false},
+		{"::1", false},
 		{"localhost", true},
 		{"287.212.12.2", false},
 	}

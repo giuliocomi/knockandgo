8f39aab4aae72c03f91f3eaafcd1afdfc658a074
giuliocomi <giulio1.comi@mail.polimi.it>
2018-08-22 17:23:44 +0200

    Now Encrypt and Decrypt are designed to return an error to the caller (client and server instances)

--- crypto/crypto_handler.go
+++ crypto/crypto_handler.go
@@ -13,57 +13,57 @@ import (
 
 var random io.Reader
 
-func Encrypt(msg string, certificate string) string {
+func Encrypt(msg string, certificate string) (string, error) {
 	random = rand.Reader
 	var pub *rsa.PublicKey
 
 	file, _ := filepath.Abs(certificate)
 	public, err := ioutil.ReadFile(file)
 	if err != nil {
-		log.Fatalf("read key file: %s", err)
+		log.Println("read key file: %v", err)
+		return "", err
 	}
 
 	pub_block, _ := pem.Decode(public)
 	pubInterface, parseErr := x509.ParsePKIXPublicKey(pub_block.Bytes)
-
 	if parseErr != nil {
-		log.Println("Load public key error")
-		panic(parseErr)
+		log.Println(parseErr)
+		return "", err
 	}
 	pub = pubInterface.(*rsa.PublicKey)
 
 	encryptedData, encryptErr := rsa.EncryptPKCS1v15(random, pub, []byte(msg))
 	if encryptErr != nil {
-		panic(encryptErr)
+		log.Println(encryptErr)
+		return "", err
 	}
-
-	return string(encryptedData)
+	return string(encryptedData), nil
 }
 
-func Decrypt(encrypted_msg string, certificate string) string {
+func Decrypt(encrypted_msg string, certificate string) (string, error) {
 	random = rand.Reader
 	var pri *rsa.PrivateKey
 
 	file, _ := filepath.Abs(certificate)
 	private, err := ioutil.ReadFile(file)
 	if err != nil {
-		log.Fatalf("read key file: %s", err)
+		log.Println("read key file: %v", err)
+		return "", err
 	}
 
 	priv_block, _ := pem.Decode(private)
 	privateKeyBlock := priv_block
 
 	pri, parseErr := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
-
 	if parseErr != nil {
-		log.Println("Load private key error")
-		panic(parseErr)
+		log.Println(parseErr)
+		return "", err
 	}
 
 	decryptedData, decryptErr := rsa.DecryptPKCS1v15(random, pri, []byte(encrypted_msg))
 	if decryptErr != nil {
 		log.Println(decryptErr)
+		return "", err
 	}
-
-	return string(decryptedData)
+	return string(decryptedData), nil
 }
